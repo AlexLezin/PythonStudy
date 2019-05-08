449c51be8778813cfa270f7f13da5f67fb8a079b
kizevetter <aleksandr.lyozin@icloud.com>
2019-05-08 21:51:02 +0300

    Changed sn74.py

--- sn74.py
+++ sn74.py
@@ -1,5 +1,4 @@
 import RPi.GPIO as IO
-
 import time
 
 IO.setwarnings(False)
@@ -13,12 +12,58 @@ IO.setup(data_pin, IO.OUT)
 IO.setup(latch_pin, IO.OUT)
 IO.setup(clock_pin, IO.OUT)
 
-def lightUp(ledNum: int) -> int:
-        print(ledNum)
+def askForAction():
+        ans1 = input("What do you want to do? (Up/Down) ")
+        if ans1 == "Up":
+                ans2 = int(input("Which one? (1-8) "))
+                lightUp(ans2)
+        elif ans1 == "Down":
+                ans2 = int(input("Which one? (1-8) "))
+                lightDown(ans2)
+
+def lightUp(ledNum: int):
+        IO.output(data_pin, 1)            # pull up the data pin for every bit.
+        time.sleep(0.1)
+        for i in range(ledNum):
+                IO.output(clock_pin, 1)            # pull CLOCK pin high
+                time.sleep(0.1)
+                IO.output(data_pin, 0)            # clear the DATA pin
+                time.sleep(0.1)
+                IO.output(clock_pin, 0)            # pull CLOCK pin down, to send a rising edge
+                time.sleep(0.1)
+        time.sleep(0.1)
+        IO.output(latch_pin, 1)            # pull the SHIFT pin high to put the 8 bit data out parallel
+        time.sleep(0.1)
+        IO.output(latch_pin, 0)            # pull down the SHIFT pin
         
-s = input('Gimme somesthing!')
-lightUp(s)
-        
+                
+
+def lightDown(ledNum: int):
+        IO.output(data_pin, 0)            # clear the DATA pin, to send 0
+        for i in range(ledNum):
+                IO.output(clock_pin, 1)            # pull CLOCK pin high
+                IO.output(clock_pin, 0)            # pull CLOCK pin down, to send a rising edge
+                IO.output(data_pin, 0)            # clear the DATA pin
+       
+        IO.output(latch_pin, 1)            # pull the SHIFT pin high to put the 8 bit data out parallel
+        IO.output(latch_pin, 0)            # pull down the SHIFT pin
+   
+
+def clearAll():
+        for i in range(8):
+                IO.output(data_pin, 0)            # clear the DATA pin, to send 0
+                time.sleep(0.1)            # wait for 100ms
+                IO.output(clock_pin, 1)            # pull CLOCK pin high
+                time.sleep(0.1)
+                IO.output(clock_pin, 0)            # pull CLOCK pin down, to send a rising edge
+                IO.output(data_pin, 0)            # keep the DATA bit low to keep the countdown
+                IO.output(latch_pin, 1)            # pull the SHIFT pin high to put the 8 bit data out parallel
+                time.sleep(0.1)
+                IO.output(latch_pin, 0)
+
+while True:
+        askForAction()
+
 '''
 while 1:                               # execute loop forever
     for y in range(8):            # loop for counting up 8 times
